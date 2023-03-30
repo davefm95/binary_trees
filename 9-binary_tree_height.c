c02a5fc5d5dbc1c92d38d6636aaa5cbbb6747705
Femi Adetoye <davefm95@gmail.com>
2023-03-30 07:58:28 -0700

    aadded

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,8 +6,9 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t lh = 0, rh = 0;
-	if (tree == NULL)
+	size_t lh, rh;
+
+	if (tree == NULL || (!tree->left && !tree->right))
 	{
 		return (0);
 	}
