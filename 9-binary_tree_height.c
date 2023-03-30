01e3bfd8c1b2738c2b223fb6a22b03a46dd47ec5
Femi Adetoye <davefm95@gmail.com>
2023-03-30 07:44:16 -0700

    addedcy

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,10 +6,18 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t lh, rh;
-	if (!tree)
+	size_t lh = 0, rh = 0;
+	if (tree == NULL)
+	{
 		return (0);
-	lh = binary_tree_height(tree->left);
-	rh = binary_tree_height(tree->right);
-	return ((lh > rh) ? lh + 1 : rh + 1);
+	}
+	else
+	{
+		lh = binary_tree_height(tree->left);
+		rh = binary_tree_height(tree->right);
+		if (lh > rh)
+			return (lh + 1);
+		else
+			return (rh + 1);
+	}
 }
