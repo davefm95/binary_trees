d668f9dc8e9380ca12a9aac82a7c2c8061e7929f
Femi Adetoye <davefm95@gmail.com>
2023-03-30 09:33:47 -0700

    added

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+/**
+ *binary_tree_nodes - counts number of nodes in a tree/subtree
+ *@tree: pointer to node
+ *Return: num nodes
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t rs, ls;
+
+	if (!tree)
+		return (0);
+	else if (tree && (tree->left || tree->right))
+		return (1);
+	else
+	{
+		ls = binary_tree_nodes(tree->left);
+		rs = binary_tree_nodes(tree->right);
+		return (ls + rs);
+	}
+}
